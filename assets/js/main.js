cef5297d80c38594c50b93a773a227ba82b865b3
paul <paul@namtech.sg>
2020-10-26 17:56:27 +0700

    create bienbao and meothi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,152 +1,45 @@
-//Scroll to top
-$(window).scroll(function () {
-  if ($(this).scrollTop() >= 500) {
-    $('#return-to-top').fadeIn(300);
-  } else {
-    $('#return-to-top').fadeOut(300);
-  }
-});
-$('#return-to-top').click(function () {
-  $('body,html').animate(
-    {
-      scrollTop: 0,
-    },
-    500
-  );
-});
-
-//Swiper
-$('.swiper-slider').each(function () {
-  let nextButton = $(this).find('.slide-button-next')[0];
-  let prevButton = $(this).find('.slide-button-prev')[0];
-  let pagination = $(this).find('.slide-pagination')[0];
-  var test = new Swiper(this, {
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 1,
-    spaceBetween: 10,
-    observer: true,
-    navigation: {
-      nextEl: nextButton,
-      prevEl: prevButton,
-    },
-    pagination: {
-      el: pagination,
-      clickable: true,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 'auto',
-        spaceBetweenSlides: 15,
-      },
-    },
-  });
-});
-
-//Swiper filter
-let listTab = document.querySelectorAll('.tablist');
-
-listTab.forEach(function (tab) {
-  tab.addEventListener('click', function () {
-    $('.tablist').removeClass('active');
-    $(this).addClass('active');
-    let id = tab.getAttribute('data-filter');
-    $('.wrap-slider .tab').removeClass('tab-active');
-    $('.wrap-slider .check' + id).addClass('tab-active');
-  });
-});
-
-var typeRoomSlider = new Swiper('.slider-typeroom', {
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 1,
-  centeredSlides: true,
-  observer: true,
-  observeParents: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 1.3,
-    },
-  },
-});
-
-var typeRoomSlider2 = new Swiper('.swiper-container', {
-  direction: 'horizontal',
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-var RoomHome = new Swiper('.swiper-roomsuite', {
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 30,
-  centeredSlides: true,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 1.2,
-    },
-  },
-});
-
-//Menu mobile
-$('.hamburger-btn').click(function (e) {
-  e.stopPropagation();
-  $(this).addClass('active');
-  $('.main-menu').addClass('active');
-});
-$('html').click(function () {
-  $('.hamburger-btn').removeClass('active');
-  $('.main-menu').removeClass('active');
-});
-
-
-//Tab list Gallery
-$('.list-cato .tab').click(function (e) {
-  $('.list-cato .tab').removeClass('active');
+var jsond = $('.list-cato .tabs').click(function (e) {
+  $('.list-cato .tabs').removeClass('active');
   $(this).addClass('active');
   let id = $(this).attr('data-tab');
-  $('.tablist').removeClass('active');
+  $('.list-items').removeClass('active');
   $('.' + id).addClass('active');
 });
-//Tab list type Room
-$('.list-type .tab-t').click(function (e) {
-  $('.list-type .tab-t').removeClass('active');
-  $(this).addClass('active');
-  let id = $(this).attr('data-tab');
-  $('.tablist-t').removeClass('active');
-  $('.' + id).addClass('active');
+$.ajax({
+  url: './assets/js/data.json',
+  dataType: 'json',
+  success: function (data) {
+    $.each(data.quest, function (i, q) {
+      $('.lythuyet').append(
+        `
+        <div class="items">
+           <div class="title"> 
+           <h3>${q.title}</h3>
+           </div>
+           <div class="describe">${q.description}</div>
+        </div>
+        `
+      );
+    });
+    $.each(data.practice, function (i, p) {
+      $('.thuchanh').append(
+        `
+        <div class="items">
+           <div class="title"><h3>${p.title}</h3></div>
+           <div class="describe">${p.description}</div>
+        </div>
+        `
+      );
+    });
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    console.log('ERROR', textStatus, errorThrown);
+  },
 });
-
+//Toggle mẹo
 $(document).ready(function () {
-  $('.lightgallery').lightGallery({
-    selector: '.item',
+  $('.list-items .items').click(function () {
+    $(this).find('.title').toggleClass('active');
+    $(this).find('.describe').toggleClass('active');
   });
 });
-//Tab list facilities
-$('.list-type .tab-f').click(function (e) {
-  $('.list-type .tab-f').removeClass('active');
-  $(this).addClass('active');
-  let id = $(this).attr('data-tab');
-  $('.tablist-f').removeClass('active');
-  $('.' + id).addClass('active');
-});
-
-$(document).ready(function () {
-  $('.lightgallery').lightGallery({
-    selector: '.item',
-  });
-});
-
